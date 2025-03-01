4d351fd44f115a12a46bf474c357721523521ba5
Nitin M <nitin.miyy@gmail.com>
2025-02-28 22:34:08 -0700

    fixed server

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,12 @@ console.log('client side javascript file is loaded')
 const lightsButton = document.querySelector('#lightsButton')
 const fanButton = document.querySelector('#fanButton')
 
+const ip = window.location.hostname
+const port = window.location.port
 lightsButton.addEventListener('click', (e) => {
     e.preventDefault()
 
-    fetch('http://localhost:3000/lights').then((response) => {
+    fetch(`http://${ip}:${port}/lights`).then((response) => {
         response.json().then((data) => {
             if (data.error) console.log(data.error)
             else {
@@ -19,7 +21,7 @@ lightsButton.addEventListener('click', (e) => {
 fanButton.addEventListener('click', (e) => {
     e.preventDefault()
 
-    fetch('http://localhost:3000/fan').then((response) => {
+    fetch(`http://${ip}:${port}/fan`).then((response) => {
         response.json().then((data) => {
             if (data.error) console.log(data.error)
             else {
--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const rpio = require('rpio')
+var rpio = require('rpio')
 const express = require('express')
 const path = require('path')
 
-const app = express()
-const public = path.join(__dirname, '../public')
+var app = express()
+const public_dir = path.join(__dirname, '../public')
 const views = path.join(__dirname, '../views')
 
 const PORT = 3000
@@ -13,32 +13,32 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 app.set('view engine', 'hbs')
 app.set('views', views)
-app.use(express.static(public))
+app.use(express.static(public_dir))
 
 const lights_pin = 16
 const fan_pin = 18
-rpio.open(lights_pin, rpio.OUTPUT, rpio.LOW)
-rpio.open(fan_pin, rpio.OUTPUT, rpio.LOW)
+rpio.open(lights_pin, rpio.OUTPUT, rpio.HIGH) // GPIO HIGH is off for this relay board
+rpio.open(fan_pin, rpio.OUTPUT, rpio.HIGH) 
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.get('/lights', (req, res) => {
     const { command } = req.body
-    rpio.write(lights_pin, !rpio.read(lights_pin))
+    rpio.write(lights_pin, rpio.read(lights_pin) ? rpio.LOW : rpio.HIGH)
     const state = (rpio.read(lights_pin)) ? 'on' : 'off'
     const message = "Lights turned to " + state
     res.status(200).send({ status: "no error" , message})
 })
 
 app.get('/fan', (req, res) => {
-    rpio.write(fan_pin, !rpio.read(fan_pin))
-    const state = (rpio.read(fan_pin)) ? 'on' : 'off'
+    rpio.write(fan_pin, rpio.read(fan_pin) ? rpio.LOW : rpio.HIGH)
+    const state = (rpio.read(fan_pin)) ? 'off' : 'on'
     const message = "fan turned to " + state
     res.status(200).send({ status: "no error" , message})
 })
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
     console.log("listening on port ", PORT)
 })
